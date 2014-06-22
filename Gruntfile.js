6e31b185a860fbcbbd8f15398697e20323352f6a
Anthony Bouch <tony@58bits.com>
2014-06-22 14:57:45 +0700

    Updated Gruntfile.js for Github.

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,10 +106,10 @@ module.exports = function(grunt) {
      * SSH and deploy settings
      */
     sshconfig: {
-      clou01: {
-        host: 'cloud01.akhua.com',
-        port: 6122,
-        username: 'tony',
+      yourserver: {
+        host: 'yourhost.com',
+        port: 22,
+        username: 'you',
         agent: process.env.SSH_AUTH_SOCK
       }
     },
@@ -117,11 +117,11 @@ module.exports = function(grunt) {
     sshexec: {
       deploy: {
         command: [
-          'cd /home/tony/public_html/sassmonster.com',
+          'cd /home/username/public_html/sassmonster.com',
           'git pull origin master'
         ].join(' && '),
         options: {
-          config: 'clou01'
+          config: 'yourserver'
         }
       }
     },
